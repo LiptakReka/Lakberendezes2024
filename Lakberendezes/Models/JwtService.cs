29b2559e50f776e022be5ef5fac6fad227b2107e
LiptakReka <liptakr@kkszki.hu>
2024-10-05 17:30:51 +0200

    Token generated and Set for users

--- Lakberendezes/Models/JwtService.cs
+++ Lakberendezes/Models/JwtService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -6,33 +7,49 @@ namespace Lakberendezes.Models
 {
     public class JwtService
     {
-        private readonly IConfiguration _config;
+        private readonly string _secretKey;
+        private readonly string _issuer;
+        private readonly string _audience;
         public JwtService(IConfiguration config)
         {
-            _config=config;
+            _secretKey = config["Jwt:Key"];
+            _issuer = config["Jwt:Issuer"];
+            _audience = config["Jwt:Audience"];
+
         }
-        public string GenerateToken(User user)
+        public string GenerateToken(User user,IList<string> roles)
         {
+            
             //1.felhasználói adatok (claims) hozzáadása
-            var claims = new[]
+            var claims = new List<Claim>();
             {
-                new Claim(JwtRegisteredClaimNames.Sub, user.id.ToString()),
-                new Claim(JwtRegisteredClaimNames.Email, user.email),
-                new Claim(ClaimTypes.Role , "User"),
+                new Claim(JwtRegisteredClaimNames.Sub, user.email);
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString());
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString());
+               
 
             };
+
+            //Felhasználói szerepkörök
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+
             //2.Titkosítás
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
             var creds= new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             //token létrehozás
             var tokens= new JwtSecurityToken(
-                issuer: _config["Jwt:Issuer"],
-                audience: _config["Jwt:Audience"],
+                issuer: _issuer,
+                audience: _audience,
                 claims: claims,
-                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_config["Jwt:ExpireMinutes"])),
-                signingCredentials: creds);
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
 
 
             return new JwtSecurityTokenHandler().WriteToken(tokens);

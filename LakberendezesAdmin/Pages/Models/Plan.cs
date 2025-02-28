7bd25b595ceeea18a3e47f8856af3766001a771a
LiptakReka <liptakr@kkszki.huu>
2025-02-28 21:48:26 +0100

    ExportUserplans, deleteproducts

---
+++ LakberendezesAdmin/Pages/Models/Plan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakberendezesAdmin.Pages.Models
+{
+    internal class Plan
+    {
+        public int id { get; set; }
+        public Guid userid { get; set; }
+        public string plandata { get; set; }
+        public DateTime createdat {  get; set; }
+    }
+}
---
+++ LakberendezesAdmin/Pages/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakberendezesAdmin.Pages.Models
+{
+    internal class User
+    {
+        public string Id {  get; set; }
+        public string Email { get; set; }
+        public string fullname {  get; set; }
+        public DateTime datet { get; set; }
+        public int AccessFailedCount { get; set; }  
+        public string ConcurrencyStamp {  get; set; }
+        public int EmailComfirmed {  get; set; }
+        public bool LockoutEnabled {  get; set; }
+        public DateTime? LockoutEnd { get; set; }
+        public string NormalizedEmail { get; set; }   
+        public string NormalizedUserName { get; set; }  
+        public string PasswordHash { get; set; }  
+        public string PhoneNumber {  get; set; }
+        public bool PhoneNumberConfirmed {  get; set; }
+        public string SecurityStamp {  get; set; }
+        public string UserName { get; set; }
+        public string ProfilePictureUrl {  get; set; }
+
+    }
+}
